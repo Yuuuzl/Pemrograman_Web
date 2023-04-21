71f0c1cb2373b1b5d495fe4840d10d42bfaee5ec
Wahyu Nur Cahyanto <wahyuc651@gmail.com>
2023-04-21 08:17:33 +0700

    2. Variabel

--- Modul 8/Latihan8php_html.php
+++ Modul 8/Latihan8php_html.php
@@ -21,6 +21,80 @@
 
     <!-- Shortcut kode PHP dalam html -->
     <p> Kode<?php echo "PHP"; ?> di HTML</p>
+
+
+    <h2> 2. Variabel </h2>
+    <!-- Membuat variabel -->
+    <!-- Variabel diidentifikasikan melalui karakter dollar dan diikuti nama variabel -->
+    <?php
+        // Deklarasi dan inisialisasi variabel
+        $nama = 'Rizki';
+        $umur = 20;
+        $tinggi = 1.7;
+        $menikah = false;
+        echo $nama;
+        echo '<br/>';
+        echo $umur;
+        echo '<br/>';
+        echo $tinggi;
+        echo '<br/>';
+        echo $menikah;
+        echo '<br/>';
+    ?>
+
+    <!-- memanfaatkan fungsi var_dump() atau print_r() untuk memudahkan pemeriksaaan variabel -->
+    <?php
+        // Deklarasi dan inisialisasi variabel
+        $nama = 'Rizki';
+        $umur = 20;
+        $tinggi = 1.7;
+        $menikah = false;
+        echo $nama;
+        echo '<br/>';
+        echo $umur;
+        echo '<br/>';
+        echo $tinggi;
+        echo '<br/>';
+        echo $menikah;
+        echo '<br/>';
+        // dumping informasi variabel
+        var_dump($nama);
+        echo '<br/>';
+        var_dump($umur);
+        echo '<br/>';
+        var_dump($tinggi);
+        echo '<br/>';
+        var_dump($menikah);
+        echo '<br/>';
+
+        print_r($nama);
+        echo '<br/>';
+        print_r($umur);
+        echo '<br/>';
+        print_r($tinggi);
+        echo '<br/>';
+        print_r($menikah);
+        echo '<br/>';
+    ?>
+
+    <!-- Menggunakan kontruksi bahasa isset() -->
+    <?php
+        // Deklarasi dan inisialisasi variabel
+        $nama = 'Rizki';
+        var_dump($nama);
+        // hasil : string(5) "Rizki"
+        
+        $umur = 20;
+        var_dump($umur);
+        // hasil : int(20)
+
+        $tinggi = 1.7;
+        var_dump($tinggi);
+        // hasil : float(1.7)
+
+        $menikah = false;
+        var_dump($menikah);
+        // hasil : bool(false)
     
 </body>
 </html>
