17d8f8af3b531166d3eefbb851cc057648c55da6
Wahyu Nur Cahyanto <wahyuc651@gmail.com>
2023-04-21 07:49:57 +0700

    1. Program PHP

---
+++ Modul 8/Latihan8php_html.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan bab 8 PHP</title>
+</head>
+<body>
+    <h2> 1. Program PHP </h2>
+    <!-- Membuat program php dan memisahkan kode php dan dokumen html -->
+    <?php
+        echo 'Kode PHP di sini';
+        // ...
+    ?>
+        <p> Dokumen HTML </p>
+    <?php
+        echo 'Kode PHP di sini';
+        // ...
+    ?>
+
+    <!-- Shortcut kode PHP dalam html -->
+    <p> Kode<?php echo "PHP"; ?> di HTML</p>
+    
+</body>
+</html>
