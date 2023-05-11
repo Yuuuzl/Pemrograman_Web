96628550e936239e6f1942f7271a11102b9e1136
Wahyu Nur Cahyanto <wahyuc651@gmail.com>
2023-05-11 18:49:08 +0700

    StudiKasus FIX

--- Modul 9/StudiKasus.php
+++ Modul 9/StudiKasus.php
@@ -1,3 +1,21 @@
+<?php                                                                                                   
+if (isset($_POST['login'])) {                                                                           // Membuat metode POST                                                                      
+    $username = $_POST['username'];                                                                     // Deklarasi variabel username dan password
+    $password = $_POST['password']; 
+    $message = "";
+    if (is_string($username) && is_string($password)) {                                                 // Cek apakah input berupa string
+        if ($username == "wahyu" && $password == "wahyu") {                                             // Cek apakah username dan password sesuai
+            $message = "<span style='color:white'>Selamat datang " . $username . "!</span>";            // Jika sesuai, tampilkan pesan
+        } else {
+            $message = "<span style='color:red'>Username atau Password Salah! Silakan Coba Lagi!</span>";// Jika tidak sesuai, tampilkan pesan
+        }
+    } else {
+        $message = "<span style='color:red'>Data tidak valid.</span>";                                   // Jika input bukan string, tampilkan pesan
+    }
+}
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -11,55 +29,81 @@
      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
      
      <!-- JS -->
-     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
 
-    <style>
-        .page-contents {
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            height: 100vh;
-            background-color: #f7f7f7;
-            padding-top : 16px;
-            padding-bottom : 16px;
+     <style>
+      body {
+       background: #f5f5f5;
       }
-        .container{
-            display: flex;
-            justify-content: center;
-            align-items:center;
-            background-color: #f7f7f7;
-            width: 100%;
-            height: 100%;
-            padding-left: 12px;
-            padding-right: 12px;
-        }
-        .login-content{
-            display: flex;
-            background-color: #fff;
-            text-align: center;
-            justify-content: center;
-            align-items: center;
-            padding-left: 26px;
-            padding-right: 26px;
-            width: 400px;
-            border-radius: 5px;
-            box-shadow: 0px 0px 6px #dedede;
-            height: 100%;
+     </style>
 
-        }
-    </style>
 </head>
+
 <body>
-    <div class="page-contents" >
-        <div class="container" >
-            <div class="login-content" >
-                
+<section class="vh-100 gradient-custom"> 
+  <div class="container py-5 h-200">
+    <div class="row d-flex justify-content-center align-items-center h-100">
+      <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+        <div class="card bg-dark text-white" style="border-radius: 1rem;">
+          <div class="card-body p-5 text-center">
 
+            <form class="mb-md-5 mt-md-4 pb-5" name="loginForm" action="<?php $_SERVER['PHP_SELF']; ?>" method="post" onsubmit="return validateForm()"> <!-- Membuat form login -->
+              <h2 class="fw-bold mb-2 text-uppercase">Login</h2>
+              <p class="text-white-50 mb-5">Please enter your login and password!</p>
 
+              <div class="form-outline form-white mb-4" style="text-align:left;" >
+                <label class="form-label" for="username">Username</label>  
+                <input type="Text" id="username" name="username" class="form-control form-control-lg" placeholder="Type your Username" />
+              </div>
+
+              <div class="form-outline form-white mb-4" style="text-align:left;">
+                <label class="form-label" for="typePasswordX">Password</label>  
+                <input type="password" name="password"id="password" class="form-control form-control-lg" placeholder="**********"/>
+              </div>
+
+              <?php if (isset($message)) : ?>                                  <!-- Menampilkan pesan -->
+                <div class="message"  style="padding-bottom:10px" >
+                  <?= $message ?>
+                </div>
+              <?php endif; ?>
+
+              <p class="small mb-5 pb-lg-2"><a class="text-white-50" href="#!">Forgot password?</a></p>
+
+              <button class="btn btn-outline-light btn-lg px-5" type="submit" name="login">Login</button>
+
+            </form>
+
+            <div>
+              <p class="mb-0">Don't have an account? <a href="#!" class="text-white-50 fw-bold">Sign Up</a>
+              </p>
             </div>
 
-
+          </div>
         </div>
+      </div>
     </div>
+  </div>
+</section>
+              
+<script> 
+        // Membuat fungsi validasi form
+        function validateForm() { 
+            const username = document.querySelector('input[name="username"]').value;  // Deklarasi variabel username dan password
+            const password = document.querySelector('input[name="password"]').value;
+
+            if (!username || !password) {                                             // Cek apakah username dan password kosong
+                alert('Username dan Password harus diisi!');        
+                document.forms["loginForm"]["username"].focus()     
+                return false;
+            }
+            if (!/^[a-zA-Z]*$/g.test(username) || !/^[a-zA-Z]*$/g.test(pasword)) {    // Cek apakah username dan password hanya berisi huruf
+                alert("Username dan Password hanya boleh berisi huruf");
+                document.forms["loginForm"]["username"].focus(); 
+                return false;
+            }
+        }
+</script>
+
 </body>
+
 </html>
